b4b0a2d53ff5b8ecfbc2bfcc0208ee53cd668433
Volker Killesreiter <killesre@gmail.com>
2016-04-15 17:24:37 +0200

    added code

---
+++ src/Plugin/CKEditorPlugin/Templates.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\codesnippet\Plugin\CKEditorPlugin\CodeSnippet.
+ */
+
+namespace Drupal\ckeditor_templates\Plugin\CKEditorPlugin;
+
+use Drupal\ckeditor\CKEditorPluginBase;
+use Drupal\Core\Url;
+use Drupal\editor\Entity\Editor;
+
+/**
+ * Defines the "templates" plugin.
+ *
+ * @CKEditorPlugin(
+ *   id = "templates",
+ *   label = @Translation("Templates"),
+ *   module = "ckeditor"
+ * )
+ */
+class Templates extends CKEditorPluginBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFile() {
+    return drupal_get_path('module', 'ckeditor_templates') . '/js/plugins/templates/plugin.js';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfig(Editor $editor) {
+    $settings = $editor->getSettings();
+    $path = drupal_get_path('module', 'ckeditor_templates');
+
+    return array(
+      'templates' => 'default',
+      'templates_files' => [
+        Url::fromUri('base:' . $path . '/templates/default.js', ['absolute' => TRUE])->toString()
+      ],
+      'templates_replaceContent' => FALSE,
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getButtons() {
+    return array(
+      'Templates' => array(
+        'label' => t('Templates'),
+        'image' => drupal_get_path('module', 'ckeditor_templates') . '/js/plugins/templates/icons/templates.png',
+      ),
+    );
+  }
+}
---
+++ templates/default.js
@@ -0,0 +1,25 @@
+﻿/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+// Register a templates definition set named "default".
+CKEDITOR.addTemplates( 'default', {
+	// The name of sub folder which hold the shortcut preview images of the
+	// templates.
+	imagesPath: CKEDITOR.getUrl( CKEDITOR.plugins.getPath( 'templates' ) + '../../../templates/images/' ),
+
+	// The templates definitions.
+	templates: [ {
+		title: 'Polaroid',
+		image: 'polaroid.gif',
+		description: 'Bild mit Untertitel im Sofortbildkamera-Stil',
+		html: '<figure class="instant">' +
+			// Use src=" " so image is not filtered out by the editor as incorrect (src is required).
+			'<img src=" " alt="" height="244" width="360" />' +
+      '<figcaption>' +
+			'<h3>Untertitel</h3>' +
+			'</figcaption>' +
+      '</figure>'
+	} ]
+} );
